a19707533531ed7c4868a247714e972ee539ad76
Kiril Kaloyanov <kaloyanov.kiril@gmail.com>
2021-10-13 18:29:35 +0300

    validation.introduction

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -2,7 +2,7 @@ const Like = (props) => {
         let classes = "fa fa-heart";
         if (!props.liked) classes += '-o';
     
-        return ( <i onClick={props.onClick} style={{ cursor: "pointer"}}className={classes} aria-hidden="true"></i> );
+        return ( <i onClick={props.onClick} style={{ cursor: "pointer"}} className={classes} aria-hidden="true"></i> );
 }
   
 export default Like;
--- src/components/common/navBar.jsx
+++ src/components/common/navBar.jsx
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import Customers from './../customers';
 
 const NavBar = () => {
     return ( 
