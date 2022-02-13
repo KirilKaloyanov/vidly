94a439c5fd5f8cd5b58d743551682a0954a0ae3c
Kiril Kaloyanov <kaloyanov.kiril@gmail.com>
2022-02-13 18:27:22 +0200

    movies table > current user extracted to the class

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -5,6 +5,7 @@ import Table from './common/table';
 import Like from "./common/like";
 
 class MoviesTable extends Component {
+    user = auth.getCurrentUser();
     columns = [
         { key: 'title', path: 'title', label: 'Title', content: movie => ( <Link 
             to={`/movies/${movie._id}`}>{movie.title}</Link>)},
@@ -14,28 +15,26 @@ class MoviesTable extends Component {
         { key: 'like', content: movie => ( <Like 
             liked={movie.liked} 
             onLike={() => this.props.onLike(movie)}/>)}
-    ];
-
-    theDeleteColumn = { 
-        key: 'delete', content: movie => ( <button 
-        onClick={() => this.props.onDelete(movie)} 
-        className="btn btn-danger btn-sm">Delete</button>)
-    };
-
-    constructor() {
-        super();
-        const user = auth.getCurrentUser();
-        if (user && user.isAdmin) this.columns.push(this.theDeleteColumn);
-    }
-
-    render() {  
-        const { movies, sortColumn, onSort} = this.props; 
+        ];
         
-            return ( 
-                <Table columns={this.columns} data={movies} sortColumn={sortColumn} onSort={onSort}/>
-            );
+        theDeleteColumn = { 
+            key: 'delete', content: movie => ( <button 
+                onClick={() => this.props.onDelete(movie)} 
+                className="btn btn-danger btn-sm">Delete</button>)
+            };
+            
+        constructor() {
+            super();
+            if (this.user && this.user.isAdmin) this.columns.push(this.theDeleteColumn);
         }
         
+        render() {  
+            const { movies, sortColumn, onSort} = this.props; 
+            
+            return ( 
+            <Table columns={this.columns} data={movies} sortColumn={sortColumn} onSort={onSort}/>
+        );
     }
+}
  
 export default MoviesTable;
